9a3f8b959585e70b69d3404152becea0329d9016
Kelvin Do <kelvin.h.do@gmail.com>
2013-07-26 21:24:29 -0700

    Finished the music player

--- play_music.py
+++ play_music.py
@@ -1,5 +1,30 @@
-import pygame
+from subprocess import call
+import os
+import operator
+import time
 
-pygame.mixer.init()
-pygame.mixer.music.load("blind_willie.mp3")
-pygame.mixer.music.play()
+path = "./music_files"
+
+while True:
+
+   music_files = os.listdir(path)
+
+   if len(music_files) == 0:
+      print "No songs found - Sleeping for 5 seconds"
+      time.sleep(5)
+
+   song_infos = []
+
+   for music_file in music_files:
+      full_path = os.path.join(path, music_file)
+      song_infos.append([full_path, os.path.getmtime(full_path)])
+
+   song_infos = sorted(song_infos, key=operator.itemgetter(1))
+
+   print song_infos
+
+   for song_info in song_infos:
+      song = song_info[0]
+      call(["mpg123", song])
+      call(["rm", song])
+      print "Done playing"
